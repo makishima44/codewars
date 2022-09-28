0e2f7c401c9cce47e4954524e3e0cb9e50695ca5
Artur <metallist15431543@gmail.com>
2022-09-28 17:59:50 +0300

    Sum of positive

---
+++ 35.js
@@ -0,0 +1,12 @@
+function positiveSum(arr) {
+    let sum = 0;
+    for(let i = 0; i <arr.length; i++) {
+      if(arr[i] > 0) {
+        sum += arr[i];
+      }
+    }
+    return sum;
+  }
+
+let x = [1,2,3,4,5];
+console.log(positiveSum(x))

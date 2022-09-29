bff74a615630cf606a04c258654d5cbb5d462671
Artur <metallist15431543@gmail.com>
2022-09-29 17:35:50 +0300

    Square(n) Sum

---
+++ 39.js
@@ -0,0 +1,9 @@
+function squareSum(numbers){
+    let answer = 0;
+    for(let i = 0; i < numbers.length; i++) {
+      answer += Math.pow(numbers[i], 2);
+    }
+    return answer ;
+  }
+
+  console.log(squareSum([1,2]));

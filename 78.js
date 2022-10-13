2793ecbe44852765f8561ff92b4844db4de2c18c
Artur <metallist15431543@gmail.com>
2022-10-13 22:07:49 +0300

    Will you make it?

---
+++ 78.js
@@ -0,0 +1,10 @@
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+   if(distanceToPump / mpg <= fuelLeft) {
+        return true;
+   } else {
+    return false;
+   }
+};
+
+  console.log(zeroFuel(50, 25, 2));
+  

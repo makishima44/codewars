7a084ec9cf8332dbcaf4dc0e9e1950ccf772724a
Artur <metallist15431543@gmail.com>
2024-10-06 11:18:18 +0300

    Money, Money, Money

--- 1.js
+++ 1.js
@@ -1,9 +1,22 @@
 // "this" is a problem
-
 function NameMe(first, last) {
   this.firstName = first;
   this.lastName = last;
   this.name = this.firstName + " " + this.lastName;
 }
 
-//-------------------------------------------------------------------------------------------------------------------//
+//-----------------------------------------------------------------------------------------------------------//
+//Money, Money, Money
+function calculateYears(principal, interest, tax, desired) {
+  let moneyForYear = principal;
+  let years = 0;
+  for (let i = 0; moneyForYear < desired; i++) {
+    let yearlyInterest = moneyForYear * interest;
+    moneyForYear += yearlyInterest - yearlyInterest * tax;
+    years++;
+  }
+
+  return years;
+}
+
+//-----------------------------------------------------------------------------------------------------------//

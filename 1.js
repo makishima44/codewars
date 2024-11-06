e2651c5bf56f0cc17566ac1b85bfec1082048274
Artur <metallist15431543@gmail.com>
2024-11-06 17:33:33 +0300

    Leap Years

--- 1.js
+++ 1.js
@@ -308,3 +308,18 @@ function rowWeights(array) {
   }
   return [firstTeam, secondTeam];
 }
+
+//-----------------------------------------------------------------------------------------------------------//
+// Leap Years
+
+function isLeapYear(year) {
+  if (year % 400 === 0) {
+    return true;
+  } else if (year % 100 === 0) {
+    return false;
+  } else if (year % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}

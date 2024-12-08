10eb61d8339759e99ff012fc9935646399a7326b
Artur <metallist15431543@gmail.com>
2024-12-08 12:39:26 +0300

    Valid Parentheses

--- 1.js
+++ 1.js
@@ -790,3 +790,22 @@ function persistence(num) {
 
   return count;
 }
+
+//-----------------------------------------------------------------------------------------------------------//
+// Valid Parentheses
+
+function validParentheses(parenStr) {
+  let count = 0;
+  for (char of parenStr) {
+    if (char === "(") {
+      count++;
+    } else if (char === ")") {
+      count--;
+    }
+    if (count < 0) {
+      return false;
+    }
+  }
+
+  return count === 0;
+}

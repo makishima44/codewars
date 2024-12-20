d8fe2d2111e1717c07e80b41d3fe325ca11f6fb6
Artur <metallist15431543@gmail.com>
2024-12-20 23:34:34 +0300

    Highest Scoring Word

--- 1.js
+++ 1.js
@@ -1016,3 +1016,52 @@ function alphabetPosition(text) {
 
   return answer.join(" ");
 }
+
+//-----------------------------------------------------------------------------------------------------------//
+// Highest Scoring Word
+
+function high(x) {
+  const alphabetValues = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    f: 6,
+    g: 7,
+    h: 8,
+    i: 9,
+    j: 10,
+    k: 11,
+    l: 12,
+    m: 13,
+    n: 14,
+    o: 15,
+    p: 16,
+    q: 17,
+    r: 18,
+    s: 19,
+    t: 20,
+    u: 21,
+    v: 22,
+    w: 23,
+    x: 24,
+    y: 25,
+    z: 26,
+  };
+
+  let wordsArr = x.toLowerCase().split(" ");
+  let scoreArr = [];
+
+  for (let word of wordsArr) {
+    let count = 0;
+
+    for (let char of word) {
+      count += alphabetValues[char];
+    }
+
+    scoreArr.push(count);
+  }
+  const maxScore = Math.max(...scoreArr);
+  return wordsArr[scoreArr.indexOf(maxScore)];
+}

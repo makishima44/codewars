3b0ac2fb09a371f8b3e66589ddab8b78095f2799
Artur <metallist15431543@gmail.com>
2025-01-02 14:28:08 +0300

    Convert string to camel case

--- 2.js
+++ 2.js
@@ -81,3 +81,20 @@ var countBits = function (n) {
 
   return count;
 };
+
+//-----------------------------------------------------------------------------------------------------------//
+// Convert string to camel case
+
+function toCamelCase(str) {
+  let arr = str.split(/[-_]/);
+
+  let answer = arr.map((word, index) => {
+    if (index === 0) {
+      return word;
+    } else {
+      return word[0].toUpperCase() + word.slice(1);
+    }
+  });
+
+  return answer.join("");
+}

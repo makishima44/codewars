226b4a218b4352bf8bdd4f28d25f216026444e9c
Artur <metallist15431543@gmail.com>
2025-03-05 15:18:45 +0300

    Parts of a list

--- 2.js
+++ 2.js
@@ -908,3 +908,16 @@ function minSum(arr) {
 
   return result;
 }
+
+//-----------------------------------------------------------------------------------------------------------//
+// Parts of a list
+
+function partlist(arr) {
+  let result = [];
+
+  for (let i = 1; i < arr.length; i++) {
+    result.push([arr.slice(0, i).join(" "), arr.slice(i).join(" ")]);
+  }
+
+  return result;
+}

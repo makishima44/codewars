3af9a97bbe1e9b1fceb6f84d0a053148ea6ee68a
Artur <metallist15431543@gmail.com>
2025-03-10 18:37:53 +0300

    Coding Meetup #4 - Higher-Order Functions Series - Find the first Python developer

--- 2.js
+++ 2.js
@@ -971,3 +971,46 @@ function isPangram(string) {
 
   return true;
 }
+
+//-----------------------------------------------------------------------------------------------------------//
+// Coding Meetup #4 - Higher-Order Functions Series - Find the first Python developer
+
+function getFirstPython(list) {
+  for (const developer of list) {
+    if (developer["language"] === "Python") {
+      return `${developer["firstName"]}, ${developer["country"]}`;
+    } else {
+    }
+  }
+
+  return "There will be no Python developers";
+}
+
+console.log(
+  getFirstPython([
+    {
+      firstName: "Mark",
+      lastName: "G.",
+      country: "Scotland",
+      continent: "Europe",
+      age: 22,
+      language: "JavaScript",
+    },
+    {
+      firstName: "Victoria",
+      lastName: "T.",
+      country: "Puerto Rico",
+      continent: "Americas",
+      age: 30,
+      language: "Python",
+    },
+    {
+      firstName: "Emma",
+      lastName: "B.",
+      country: "Norway",
+      continent: "Europe",
+      age: 19,
+      language: "Clojure",
+    },
+  ])
+);

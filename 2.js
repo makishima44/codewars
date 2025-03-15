7f42fe11ec3238e58f7b17ff8c598b7d3729f987
Artur <metallist15431543@gmail.com>
2025-03-15 12:56:27 +0300

    Elevator Distance

--- 2.js
+++ 2.js
@@ -1017,3 +1017,20 @@ function vowel2index(str) {
     .map((el, index) => (regex.test(el) ? index + 1 : el))
     .join("");
 }
+
+//-----------------------------------------------------------------------------------------------------------//
+// Elevator Distance
+
+function elevatorDistance(array) {
+  let result = 0;
+
+  for (let i = 0; i < array.length - 1; i++) {
+    if (array[i] > array[i + 1]) {
+      result += array[i] - array[i + 1];
+    } else {
+      result += array[i + 1] - array[i];
+    }
+  }
+
+  return result;
+}

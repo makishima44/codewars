591322add79145caac7185a850b6ab0de42c2a54
Artur <metallist15431543@gmail.com>
2025-03-19 20:26:04 +0300

    Numbers to Letters

--- 2.js
+++ 2.js
@@ -1058,3 +1058,42 @@ function evenLast(numbers) {
 
   return count ? count * numbers[numbers.length - 1] : 0;
 }
+
+//-----------------------------------------------------------------------------------------------------------//
+// Numbers to Letters
+
+function switcher(x) {
+  const alphabet = {
+    1: "z",
+    2: "y",
+    3: "x",
+    4: "w",
+    5: "v",
+    6: "u",
+    7: "t",
+    8: "s",
+    9: "r",
+    10: "q",
+    11: "p",
+    12: "o",
+    13: "n",
+    14: "m",
+    15: "l",
+    16: "k",
+    17: "j",
+    18: "i",
+    19: "h",
+    20: "g",
+    21: "f",
+    22: "e",
+    23: "d",
+    24: "c",
+    25: "b",
+    26: "a",
+    27: "!",
+    28: "?",
+    29: " ",
+  };
+
+  return x.map((el) => alphabet[Number(el)]).join("");
+}

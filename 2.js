e554f0c02d5676d685aed7903c5162d0510be497
Artur <metallist15431543@gmail.com>
2025-03-20 19:36:29 +0300

    Consecutive items

--- 2.js
+++ 2.js
@@ -1097,3 +1097,14 @@ function switcher(x) {
 
   return x.map((el) => alphabet[Number(el)]).join("");
 }
+
+//-----------------------------------------------------------------------------------------------------------//
+// Consecutive items
+
+function consecutive(arr, a, b) {
+  for (let i = 0; i < arr.length; i++) {
+    if ((arr[i] === a && arr[i + 1] === b) || (arr[i] === b && arr[i + 1] === a)) return true;
+  }
+  return false;
+}
+

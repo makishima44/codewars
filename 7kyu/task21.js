309d4a1531995637e88d25dcf4b25fd21c9dc418
Artur <metallist15431543@gmail.com>
2025-05-16 20:42:08 +0300

    Length and two values.

---
+++ 7kyu/task21.js
@@ -0,0 +1,10 @@
+function alternate(n, firstValue, secondValue) {
+  let result = [];
+
+  for (let i = 0; i < n; i++) {
+    if (i % 2 === 0) result.push(firstValue);
+    else result.push(secondValue);
+  }
+
+  return result;
+}

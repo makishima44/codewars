97d5079fa3c2bd8a64c91de6bb578f1bd4100d74
Artur <metallist15431543@gmail.com>
2025-06-13 18:26:23 +0300

    Band name generator

---
+++ 7kyu/task50.js
@@ -0,0 +1,4 @@
+function bandNameGenerator(str) {
+  if (str[0] !== str[str.length - 1]) return "The " + str[0].toUpperCase() + str.slice(1);
+  else return str[0].toUpperCase() + str.slice(1) + str.slice(1);
+}

b0c475b9fc92f0fc8c8ff1a24247acf0d932ba0a
Artur <metallist15431543@gmail.com>
2025-07-13 17:33:53 +0300

    Training JS #14: Methods of Number object--toString() and toLocaleString()

---
+++ js/8kyu/task10.js
@@ -0,0 +1,8 @@
+function colorOf(r, g, b) {
+  const hexR = r.toString(16).padStart(2, "0");
+  const hexG = g.toString(16).padStart(2, "0");
+  const hexB = b.toString(16).padStart(2, "0");
+  return `#${hexR}${hexG}${hexB}`;
+}
+
+

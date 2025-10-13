10f001656674ff4c59af93cce6bc2729f74461c5
Artur <metallist15431543@gmail.com>
2025-10-13 18:20:18 +0300

    feat: add No ifs no buts solution

---
+++ js/7kyu/NoIfsNoButs.js
@@ -0,0 +1,13 @@
+// Task: No ifs no buts
+// Link: https://www.codewars.com/kata/592915cc1fad49252f000006/train/javascript
+
+function noIfsNoButs(a, b) {
+  switch (true) {
+    case a > b:
+      return `${a} is greater than ${b}`;
+    case a < b:
+      return `${a} is smaller than ${b}`;
+    default:
+      return `${a} is equal to ${b}`;
+  }
+}

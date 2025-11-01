fbba2aea17fc91a2276ccaadd1cf8c6d6b858166
Artur <metallist15431543@gmail.com>
2025-11-01 12:40:41 +0300

    feat: add Convert the score solution

---
+++ js/7kyu/convertTheScore.js
@@ -0,0 +1,22 @@
+// Task: Convert the score
+// Link: https://www.codewars.com/kata/5b6c220fa0a661fbf200005d/train/javascript
+
+function scoreboard(string) {
+  const numbers = {
+    nil: 0,
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  };
+
+  return string
+    .split(" ")
+    .filter((el) => el in numbers)
+    .map((el) => numbers[el]);
+}

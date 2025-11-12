093a31cd992440a36b876a751e4098d18cb01b07
Artur <metallist15431543@gmail.com>
2025-11-12 20:51:38 +0300

    fix: refactor Shared Bit Counter

--- js/7kyu/sharedBitCounter.js
+++ js/7kyu/sharedBitCounter.js
@@ -2,9 +2,6 @@
 // Link: https://www.codewars.com/kata/58a5aeb893b79949eb0000f1/train/javascript
 
 function sharedBits(a, b) {
-  const binaryA = a.toString(2);
-  const binaryB = b.toString(2);
-
   const commonBits = a & b;
 
   let count = 0;
@@ -17,5 +14,3 @@ function sharedBits(a, b) {
 
   return count >= 2;
 }
-
-

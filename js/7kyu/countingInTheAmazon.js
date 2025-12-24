a71cb1a9c444804af1e1fc3f549bc4b230240317
Artur <metallist15431543@gmail.com>
2025-12-24 18:24:28 +0300

    feat: add Counting in the Amazon solution

---
+++ js/7kyu/countingInTheAmazon.js
@@ -0,0 +1,6 @@
+// Task: Counting in the Amazon
+// Link: https://www.codewars.com/kata/55b95c76e08bd5eef100001e/train/javascript
+
+function countArara(n) {
+  return n % 2 === 0 ? "adak ".repeat(n / 2).trim() : "adak ".repeat(n / 2) + "anane";
+}

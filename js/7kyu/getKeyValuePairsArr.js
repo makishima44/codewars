5abd655bbe4be8c26c652599a3841c3ae377b97c
Artur <metallist15431543@gmail.com>
2026-01-02 14:55:39 +0300

    feat: add Get key/value pairs as arrays solution

---
+++ js/7kyu/getKeyValuePairsArr.js
@@ -0,0 +1,6 @@
+// Task: Get key/value pairs as arrays
+// Link: https://www.codewars.com/kata/515dfd2f1db09667a0000003/train/javascript
+
+function keysAndValues(data) {
+  return [Object.keys(data), Object.values(data)];
+}

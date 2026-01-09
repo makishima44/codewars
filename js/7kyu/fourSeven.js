a3ab517b49aa3d83de30e37418c5ff4ac2d3cd6d
Artur <metallist15431543@gmail.com>
2026-01-09 12:05:58 +0300

    feat: add Four/Seven solution

---
+++ js/7kyu/fourSeven.js
@@ -0,0 +1,7 @@
+// Task: Four/Seven
+// Link: https://www.codewars.com/kata/5ff50f64c0afc50008861bf0/train/javascript
+
+function fourSeven(n) {
+  const obj = { 4: 7, 7: 4 };
+  return obj[n] || 0;
+}

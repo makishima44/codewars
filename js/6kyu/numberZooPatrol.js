e10d2223dbe78aaace17c2b1338195e8138f144c
Artur <metallist15431543@gmail.com>
2026-02-13 20:31:12 +0300

    feat: add Number Zoo Patrol solution

---
+++ js/6kyu/numberZooPatrol.js
@@ -0,0 +1,22 @@
+// Task: Number Zoo Patrol
+// Link: https://www.codewars.com/kata/5276c18121e20900c0000235/train/javascript
+
+function findNumber(nums) {
+  const n = nums.length;
+
+  for (let i = 0; i < n; i++) {
+    while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] !== nums[i]) {
+      let temp = nums[nums[i] - 1];
+      nums[nums[i] - 1] = nums[i];
+      nums[i] = temp;
+    }
+  }
+
+  for (let i = 0; i < n; i++) {
+    if (nums[i] !== i + 1) {
+      return i + 1;
+    }
+  }
+
+  return n + 1;
+}

17ebb2b964aa39a2880b05b50e73ba29f79df5f9
Artur <metallist15431543@gmail.com>
2026-03-16 18:53:33 +0300

    feat: add Ho Ho Ho with Functions! solution

---
+++ js/6kyu/hoHoHoWithFunction.js
@@ -0,0 +1,11 @@
+// Task: Array Deep Count
+// Link: https://www.codewars.com/kata/596f72bbe7cd7296d1000029/train/javascript
+
+function ho(str) {
+  if (str === undefined) {
+    return "Ho!";
+  }
+  return "Ho " + str;
+}
+
+

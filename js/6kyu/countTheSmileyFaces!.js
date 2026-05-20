ce4ba720a63710e63136fe0e50e4b11e6569d60c
Artur <metallist15431543@gmail.com>
2026-05-20 18:13:03 +0300

    feat: add Count the smiley faces! solution

---
+++ js/6kyu/countTheSmileyFaces!.js
@@ -0,0 +1,7 @@
+// Task: Count the smiley faces!
+// https://www.codewars.com/kata/583203e6eb35d7980400002a/train/javascript
+
+function countSmileys(arr) {
+  return arr.filter((face) => /^[:;][-~]?[)D]$/.test(face)).length;
+}
+
